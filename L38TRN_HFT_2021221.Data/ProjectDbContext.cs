635d2e7e541054649828a567140b80cb4492a9ff
L38TRN@stud.uni-obuda.hu <L38TRN@stud.uni-obuda.hu>
2021-10-17 20:54:09 +0200

    ADD: Added rows to the Songs Table

--- L38TRN_HFT_2021221.Data/ProjectDbContext.cs
+++ L38TRN_HFT_2021221.Data/ProjectDbContext.cs
@@ -33,11 +33,15 @@ namespace L38TRN_HFT_2021221.Data
 
             Artist kendrickLamar = new Artist() { ArtistID = 1, ArtistName = "Kendrik Lamar" };
             Album goodKid = new Album() { AlbumID = 1, AlbumName = "Good Kid, M.A.A.D City", ArtistName = kendrickLamar };
-            Song backseatFreestlye = new Song() { SongID = 1, AlbumName = goodKid, ArtistName = kendrickLamar, Duration = 212};
+            Song backseatFreestlye = new Song() { SongID = 1, SongName = "Backseat Freestyle", AlbumName = goodKid, ArtistName = kendrickLamar, Duration = 212};
+            Song poeticJustice = new Song() { SongID = 2, SongName = "Poetic Justice", AlbumName = goodKid, ArtistName = kendrickLamar, Duration = 212};
+            Song moneyTrees = new Song() { SongID = 3, SongName = "Money Trees", AlbumName = goodKid, ArtistName = kendrickLamar, Duration = 212};
+            Song swimmingPools = new Song() { SongID = 4, SongName = "Swimming Pools (Drank)", AlbumName = goodKid, ArtistName = kendrickLamar, Duration = 212};
+            Song compton = new Song() { SongID = 5, SongName = "Compton", AlbumName = goodKid, ArtistName = kendrickLamar, Duration = 212};
 
             modelBuilder.Entity<Album>().HasData(kendrickLamar);
             modelBuilder.Entity<Artist>().HasData(goodKid);
-            modelBuilder.Entity<Song>().HasData(backseatFreestlye);
+            modelBuilder.Entity<Song>().HasData(backseatFreestlye); 
 
         }
     }
