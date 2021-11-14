94e3d59dfa7ec4785bc29575eb7e82787e3122fa
L38TRN@stud.uni-obuda.hu <L38TRN@stud.uni-obuda.hu>
2021-11-14 19:53:23 +0100

    ADD: Added repositories

---
+++ L38TRN_HFT_2021221.Repository/Repository.cs
@@ -0,0 +1,25 @@
+﻿using L38TRN_HFT_2021221.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L38TRN_HFT_2021221.Repository
+{
+    public abstract class Repository<T> 
+    {
+        protected ProjectDbContext projectDbContext;
+        public Repository(ProjectDbContext projectDbContext)
+        {
+            this.projectDbContext = projectDbContext;
+        }
+
+        public IQueryable<T> GetAll()
+        {
+            return projectDbContext.Set<T>(); // set => halmaz; != beállítás
+        }
+
+        public abstract T GetOne(int id);
+    }
+}
